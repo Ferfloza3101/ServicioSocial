0d0e1f63fb5f7630f11e536ca2a031b0ff434c05
Ferfloza3101 <ferfloza2003@gmail.com>
2025-02-19 15:56:06 -0600

    Delete HolaMundo.py

--- HolaMundo.py
+++
@@ -1,7 +0,0 @@
-#Mostrar cada carácter de la cadena “Hola mundo”.
-
-cadena = "Hola Mundo"   #Creamos nuestra cadena.
-
-for caracter in cadena:     #Como en el ej anterior usamos for para mostrar dato por dato
-    print(caracter)     #Imprimimos caracter x caracyter 
-
