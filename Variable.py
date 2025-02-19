6664b1464a16aba5d573b28b1dc4a5a35ee3dd75
Ferfloza3101 <ferfloza2003@gmail.com>
2025-02-19 15:54:54 -0600

    Delete Variable.py

--- Variable.py
+++
@@ -1,9 +0,0 @@
-#Crea una variable numerica y si la variable esta entre 0 y 10, mostrar un mensaje indicandolo
-
-varnum = 7 #Aqui introducimos la variable numerica
-
-if 0 <= varnum <= 10:
-    print ("El numero esta entre 0 y 10")   #Condicionamos que si el numero estra entre 0 y 10 imprim el mensaje correcto
-
-else:
-    print ("Tu numero no esta entre 0 y 10")    #Si no cumple nuestra condicion mostramos mensaje negando
